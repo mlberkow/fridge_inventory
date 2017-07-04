a1a75522be1e2b03671f16dfcc8636010d88c961
Mike Berkow <mike@mikeberkow.com>
2017-07-04 13:32:16 -0600

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+plain_proteins = [
+	'Chicken (cooked, bags)', 'Chicken (uncooked)', 'Chicken Broth', 'Buffalo', 'Buffalo Broth',
+		'Grnd Beef (uncooked)', 'Turkey (uncooked, bags)', 'Turkey Broth', 'Tilapia (Bags),', 'Other Fish'
+	]
+
+starches = [
+	'Bulgar', 'Kasha', 'Quinoa', 'White Rice', 'Wild Rice'
+	]
+
+sauces = [
+	'Pasta Sauce (No meat)', 'Pasta Sauce (Meat)', 'Pizza Sauce'
+	]
+
+beef = [
+	'Beer Braised Beef', 'Beef Barley Soup', 'Meat Pies', 'BB Bison Chuck Roast'
+	]
+
+chicken = [
+	'Indian 1', 'Indian 2', 'Almond', 'BBQ', 'Bourbon', 'Cacciatore', '5 Spice', 'Ginger', 'Hawaiian', 'Honey Sesame',
+	'Lemon/Mustard', 'Moroccan', 'Orange Cashew', 'Parmesan', 'Shwarma', 'Soup', 'Tagine', 'Thai Peanut'
+	]
+
+turkey = [
+	'Apple', 'Balsamic', 'Guinness', 'Maple Walnut', 'Maple Mustard', 'Mustard Lime'
+	]
+
+plain_proteins.each do |content|
+	Recipe.create(content: content, food_type: 'Plain Protein')
+end
+
+starches.each do |content|
+	Recipe.create(content: content, food_type: 'Starch')
+end
+
+sauces.each do |content|
+	Recipe.create(content: content, food_type: 'Sauces')
+end
+
+beef.each do |content|
+	Recipe.create(content: content, food_type: 'Beef')
+end
+
+chicken.each do |content|
+	Recipe.create(content: content, food_type: 'Chicken')
+end
+
+turkey.each do |content|
+	Recipe.create(content: content, food_type: 'Turkey')
+end
