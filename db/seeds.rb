ff03e922342b4455e0c764302d13d682610184b1
Mike Berkow <mike@mikeberkow.com>
2017-07-08 20:39:27 -0600

    formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,25 +32,25 @@ turkey = [
 	]
 
 plain_proteins.each do |content|
-	Recipe.create(content: content, food_type: 'Plain Protein')
+	Recipe.create(content: content, food_type: 'Plain Protein', quantity_upstairs: 0, quantity_downstairs: 0)
 end
 
 starches.each do |content|
-	Recipe.create(content: content, food_type: 'Starch')
+	Recipe.create(content: content, food_type: 'Starch', quantity_upstairs: 0, quantity_downstairs: 0)
 end
 
 sauces.each do |content|
-	Recipe.create(content: content, food_type: 'Sauces')
+	Recipe.create(content: content, food_type: 'Sauces', quantity_upstairs: 0, quantity_downstairs: 0)
 end
 
 beef.each do |content|
-	Recipe.create(content: content, food_type: 'Red Meat')
+	Recipe.create(content: content, food_type: 'Red Meat', quantity_upstairs: 0, quantity_downstairs: 0)
 end
 
 chicken.each do |content|
-	Recipe.create(content: content, food_type: 'Chicken')
+	Recipe.create(content: content, food_type: 'Chicken', quantity_upstairs: 0, quantity_downstairs: 0)
 end
 
 turkey.each do |content|
-	Recipe.create(content: content, food_type: 'Turkey')
+	Recipe.create(content: content, food_type: 'Turkey', quantity_upstairs: 0, quantity_downstairs: 0)
 end
